2da41c9e76987d117efc2f11c25be878e325e141
JonathanAndrade19 <jonathanjampamb@gmail.com>
2021-03-12 11:17:57 -0300

    new functions inserting in funcoes.php

--- Avancando/Banco.php
+++ Avancando/Banco.php
@@ -1,55 +1,48 @@
 <?php
+// require_once garante que esse arquivo será tránsferido apenas uma vez.
+require_once 'funcoes.php';
 
-function sacar(array $conta, float $valorASacar): array
-{
-    if ( $valorASacar > $conta['Saldo'] ) {
-        exibeMensagem('Você não tem saldo suficiente');
-    } else {
-        $conta['Saldo'] -= $valorASacar;
-    }
-    return $conta;
-}
-
-function depositar(array $conta,float $valorADepositar): array
-{  
-    if ($valorADepositar > 0) {
-        $conta['Saldo'] += $valorADepositar;
-        return $conta;
-    }
-
-    exibeMensagem("O Valor do Deposito e Invalido");
-    
-}
-
-function exibeMensagem(string $mensagem) 
-{
-    // Uma SubRotina
-    echo $mensagem . '</br>';
-}
-
+// Array Associativos.
 $contaCorrente = [ 
     10509439488 => [ 
         'Codigo' => 1, 
-        'NomeConta' => 'Jonathan Moreira', 
+        'titular' => 'Jonathan Moreira', 
         'Saldo' => 451
     ],
     70231322754 => [ 
         'Codigo' => 2, 
-        'NomeConta' => 'Lídia Araújo', 
+        'titular' => 'Lídia Araújo', 
         'Saldo' => 488
     ],
     11148759420 => [ 
         'Codigo' => 3, 
-        'NomeConta' => 'Liv Andrade', 
+        'titular' => 'Liv Andrade', 
         'Saldo' => 357
+    ],
+    12345678910 => [
+        'codigo' => 4,
+        'titular' => 'Nathan Andrade',
+        'Saldo' => 378
     ]
 ];
 
+// função de sacar um valor da conta.
 $contaCorrente[10509439488] = sacar($contaCorrente[10509439488], 151);
 
+// função de depositar um valor a conta. 
 $contaCorrente[70231322754] = depositar($contaCorrente[70231322754], 12);
 
+// função que deixa as letras Maiusculas.
+titularComLetrasMaiusculas($contaCorrente[10509439488]);
+
+// unset Usado para excluir um indice de uma arrey, ou informações passadas.
+unset($contaCorrente[12345678910]);
+
+// listando com o foreach todos as contas existente dentro do array associativo.
 foreach ($contaCorrente as $cpf => $contas) {
-    exibeMensagem('');
-    exibeMensagem( ' CPF: ' . $cpf . ' Nome: ' . $contas['NomeConta'] . ' Saldo: ' . $contas['Saldo']);
+    // function List php 7.1 ->
+    ['titular' => $titular, 'Saldo' => $saldo] = $contas;
+
+    // subrotina axibeMensagem. uma função básica que serve apenas para fazer um quebrar de linha 
+    exibeMensagem( ' CPF: ' . $cpf . ' Nome: ' . $titular . ' Saldo: ' . $saldo);
 }
---
+++ Avancando/funcoes.php
@@ -0,0 +1,37 @@
+<?php 
+
+// Função que trás dois valores como parametro, uma função que serve para sacar valor de uma conta.
+function sacar(array $conta, float $valorASacar): array
+{
+    if ( $valorASacar > $conta['Saldo'] ) {
+        exibeMensagem('Você não tem saldo suficiente');
+    } else {
+        $conta['Saldo'] -= $valorASacar;
+    }
+    return $conta;
+}
+
+// Função que trás dois valores como parametro, uma função que serve para sacar valor de uma conta.
+function depositar(array $conta,float $valorADepositar): array
+{  
+    if ($valorADepositar > 0) {
+        $conta['Saldo'] += $valorADepositar;
+        return $conta;
+    }
+
+    exibeMensagem("O Valor do Deposito e Invalido");
+    
+}
+
+// Subrotina
+function exibeMensagem(string $mensagem) 
+{
+    // Uma SubRotina
+    echo $mensagem . '</br>';
+}
+
+// Função que serve para deixar o nome do titular da conta sempre em maiusculo, usando a variavel STRTOUPPER.
+function titularComLetrasMaiusculas(array &$conta)
+{
+    $conta['titular'] = strtoupper($conta['titular']);
+}
