caae51830a76e29c687d83b023a4aa6744f7b183
JonathanAndrade19 <jonathanjampamb@gmail.com>
2021-03-14 09:09:58 -0300

    new class Pessoa

--- Banco/src/Endereco.php
+++ Banco/src/Endereco.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 
 class Endereco
 {
-    private $cidade;
-    private $bairro;
-    private $rua;
-    private $numero;
+    private string $cidade;
+    private string $bairro;
+    private string $rua;
+    private string $numero;
 
     public function __construct(string $cidade, string $bairro, string $rua, string $numero)
     {
@@ -19,20 +19,19 @@ class Endereco
     {
         return $this->cidade;
     }
-    
+
     public function recuperaBairro(): string
     {
         return $this->bairro;
     }
-    
+
     public function recuperaRua(): string
     {
         return $this->rua;
     }
-    
+
     public function recuperaNumero(): string
     {
         return $this->numero;
     }
-
 }
--- Banco/src/Funcionario.php
+++ Banco/src/Funcionario.php
@@ -1,12 +1,13 @@
 <?php 
 
-class Funcionario 
+class Funcionario extends Pessoa
 {
 
     private $cargo;
 
-    public function __construct(string $cargo)
+    public function __construct(string $nome , CPF $cpf , string $cargo)
     {
+        parent::__construct($nome, $cpf);
         $this->cargo = $cargo;
     }
 
@@ -15,4 +16,9 @@ class Funcionario
         return $this->cargo;
     }
 
+    public function alteraNome(string $nome): void
+    {
+        $this->validaNomeNome($nome);
+        $this->nome = $nome;
+    }
 }
